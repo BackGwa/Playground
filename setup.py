fcf8d7b2d07b20bd445f82194029462cfdc4d857
BackGwa <64456214+BackGwa@users.noreply.github.com>
2022-11-08 17:52:42 +0900

    package setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(  name='playground-python',
+        version='1.0.0.0',
+        description='Playground는 학교와 학생을 위한 \'Python 라이브러리\' 입니다.',
+        author='BACKGWA',
+        author_email='backgwa@icloud.com',
+        url='https://github.com/BackGwa/Playground',
+        license='BSD3',
+        py_modules=['package'],
+        python_requires='>=3',
+        install_requires=[],
+        packages=['package']
+)
